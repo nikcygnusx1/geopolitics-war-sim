ea3be7312a39d23bad8e9cc37630610aa5bd5571
nikcygnusx1 <nikhil.cygnusx1@gmail.com>
2026-06-17 19:18:12 +0530

    refactor: rename LeaderDossiersView component file
    Update import path to match the simplified naming convention for the LeaderDossiers component.

--- src/components/panels/IntelPanel.tsx
+++ src/components/panels/IntelPanel.tsx
@@ -6,7 +6,7 @@ import { audio } from '../../utils/audio';
 import { useUIStore } from '../../store/uiStore';
 import AnimatedValue from '../shared/AnimatedValue';
 import ArachnePanel from './ArachnePanel';
-import LeaderDossiersView from './leaderDossiersView';
+import LeaderDossiersView from './leaderDossiers';
 import { useMirrorStore } from '../../store/mirrorStore';
 
 export default function IntelPanel() {
